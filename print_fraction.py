2d107b0c5d18dcd263a744e0d3ea904e0dbcb3d8
Jalaluddin Khan <jalaluddinkhan2k2@hmail.com>
2022-11-22 00:17:02 +0530

    new

--- print_fraction.py
+++ print_fraction.py
@@ -1,9 +1,16 @@
-class fration:
+class fration: # adding two frations
     def __init__(self,n,d):
         self.num=n
         self.dec=d
     def __str__(self):
-        return "{}/{}".format(self.num,self.dec)
+        return " xx {}/{}".format(self.num,self.dec)
+    def __add__(self,ot):
+        s= ot.num * self.dec + self.num * ot.dec
+        d= self.dec * self.dec
+
+        return " =>  {}/{}".format(s,d)
 
 n=fration(10,20)
-print(n)
+x=fration(2,5)
+print(x)
+print(n+x)
